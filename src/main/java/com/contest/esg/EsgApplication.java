9ca5ce6ccf0135763bb259f725e0013d6f2034e3
min429 <lsm990429@gmail.com>
2024-05-08 22:26:02 +0900

    Initial commit

---
+++ src/main/java/com/contest/esg/EsgApplication.java
@@ -0,0 +1,12 @@
+package com.contest.esg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgApplication.class, args);
+	}
+}
